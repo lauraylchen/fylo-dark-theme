253688ad50a7d91134f01d33c70a930d4c6aa3b5
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-16 23:00:36 -0400

    Added all content needed into Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,32 @@
+import logo from '../images/logo.svg';
+import illustrationIntro from '../images/illustration-intro.png';
+import bgCurvyMobile from '../images/bg-curvy-mobile.svg';
+
 const Hero = () => {
   return (
-    <header></header>
+    <header>
+      <div className="bg-darkBlue">
+        <nav>
+          <div className='flex justify-between'>
+            <img src={logo} alt='Fylo Company Logo' />
+            <div className="flex font-raleway">
+              <a href="/">Features</a>
+              <a href="/">Team</a>
+              <a href="/">Sign In</a>
+            </div>
+          </div>
+        </nav>
+        <img src={illustrationIntro} alt="Banner Illustration" />
+        <img src={bgCurvyMobile} className='w-full' alt="Curvy Illustration Background" />
+      </div>
+      <div className='bg-darkBlueMain'>
+        <h2>All your files in one secure location, accessible anywhere.</h2>
+        <p>
+          Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.
+        </p>
+        <button>Get Started</button>
+      </div>
+    </header>
   );
 }
 
