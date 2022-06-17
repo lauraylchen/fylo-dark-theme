60e817dd47ee642dea4c21d11bc73db70a5f361d
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-16 22:11:31 -0400

    Set up colors for challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkBlue': '#1C2431',
+        'darkBlueMain': '#181F2A',
+        'darkBlueFooter': '#0B1523',
+        'darkBlueComment': '#202A3C',
+        'cyan': '#65E2D9',
+        'blue': '#339ECC',
+        'lightRed': '#FF4242',
+        'white': '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 }
